1950fc956a853c36288de4a7a142c69d738009d5
Brittany Lau <brittanyplau@gmail.com>
2021-02-26 01:01:31 -0500

    Create stub login, registration, and 404 pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
+import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { AuthContext } from "./context/auth";
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound";
+import PrivateRoute from "./PrivateRoute";
+import Registrations from "./pages/Registrations";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthContext.Provider value={false}>
+      <Router>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <PrivateRoute exact path="/" component={Registrations} />
+          <Route exact path="*" component={NotFound} />
+        </Switch>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
---
+++ src/pages/Registrations.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Registrations() {
+  return (
+    <div>
+      <h1>Main Registration</h1>
+    </div>
+  );
+}
+
+export default Registrations;
