4a61c0c7abcf2161d17f2e9de7a8b9bd0184eeb0
Brittany Lau <brittanyplau@gmail.com>
2021-02-26 00:19:35 -0500

    Configure ESLint and Prettier

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/jsx-filename-extension */
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
