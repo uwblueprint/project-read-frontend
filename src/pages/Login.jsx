6b2d5edb7658f06928b29ce621fbe1e7c382c9b1
Daniel <daniel.chen115@gmail.com>
2021-03-27 11:28:56 -0400

    Remove unused auth providers

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,11 +9,7 @@ function Login() {
 
   const uiConfig = {
     signInFlow: "popup",
-    signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    ],
+    signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
     callbacks: {
       signInSuccess: () => false,
     },
