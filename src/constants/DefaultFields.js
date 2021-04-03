8e212bb9e363555e824ed1bd070c37241c823b8d
Brittany Lau <brittanyplau@gmail.com>
2021-04-03 14:30:58 -0400

    Implement feedback

---
+++ src/constants/DefaultFields.js
@@ -0,0 +1,28 @@
+const DefaultFields = Object.freeze({
+  EMAIL: {
+    name: "email",
+    label: "Email",
+  },
+  FIRST_NAME: {
+    name: "first_name",
+    label: "First Name",
+  },
+  LAST_NAME: {
+    name: "last_name",
+    label: "Last Name",
+  },
+  NUM_CHILDREN: {
+    name: "num_children",
+    label: "No. of Children",
+  },
+  PHONE: {
+    name: "phone_number",
+    label: "Phone Number",
+  },
+  PREFERRED_CONTACT: {
+    name: "preferred_comms",
+    label: "Preferred Contact",
+  },
+});
+
+export default DefaultFields;
---
+++ src/constants/registration/RegistrationTableColumns.js
@@ -0,0 +1,36 @@
+import DefaultFields from "../DefaultFields";
+
+const RegistrationTableColumns = [
+  {
+    name: DefaultFields.FIRST_NAME.name,
+    label: DefaultFields.FIRST_NAME.label,
+    options: { filter: false },
+  },
+  {
+    name: DefaultFields.LAST_NAME.name,
+    label: DefaultFields.LAST_NAME.label,
+    options: { filter: false },
+  },
+  {
+    name: DefaultFields.EMAIL.name,
+    label: DefaultFields.EMAIL.label,
+    options: { filter: false },
+  },
+  {
+    name: DefaultFields.PHONE.name,
+    label: DefaultFields.PHONE.label,
+    options: { filter: false },
+  },
+  {
+    name: DefaultFields.NUM_CHILDREN.name,
+    label: DefaultFields.NUM_CHILDREN.label,
+    options: { searchable: false, filterOptions: { fullWidth: true } },
+  },
+  {
+    name: DefaultFields.PREFERRED_CONTACT.name,
+    label: DefaultFields.PREFERRED_CONTACT.label,
+    options: { searchable: false, filterOptions: { fullWidth: true } },
+  },
+];
+
+export default RegistrationTableColumns;
