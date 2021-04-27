5aff1d194237619512b465724c2cc17ea1e65373
Brittany Lau <brittanyplau@gmail.com>
2021-04-26 23:52:10 -0400

    Display parent field questions

--- src/components/registration/RegistrationForm.jsx
+++ src/components/registration/RegistrationForm.jsx
@@ -1,8 +1,47 @@
-import React from "react";
-import { Typography } from "@material-ui/core";
+import React, { useContext } from "react";
+import { makeStyles, TextField, Typography } from "@material-ui/core";
+import { FieldsContext } from "../../context/fields";
+
+const useStyles = makeStyles((theme) => ({
+  formField: {
+    marginBottom: theme.spacing(1),
+    marginTop: theme.spacing(1),
+    width: 328,
+  },
+}));
+
+function useFormFields(initialValues) {
+  const [formFields, setFormFields] = React.useState(initialValues);
+  const createChangeHandler = (key) => (e) => {
+    const { value } = e.target;
+    setFormFields((prev) => ({ ...prev, [key]: value }));
+  };
+  return { formFields, createChangeHandler };
+}
 
 function RegistrationForm() {
-  return <Typography>Currently enrolling a new family</Typography>;
+  const classes = useStyles();
+  const { parentFields } = useContext(FieldsContext);
+  const { formFields, createChangeHandler } = useFormFields(
+    Object.assign({}, ...parentFields.map((field) => ({ [field.id]: "" })))
+  );
+
+  return (
+    <>
+      <Typography>Currently enrolling a new family</Typography>
+      {parentFields.map((value) => (
+        <div>
+          <TextField
+            label={value.name}
+            value={formFields[value.id]}
+            onChange={createChangeHandler(value.id)}
+            variant="outlined"
+            className={classes.formField}
+          />
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default RegistrationForm;
