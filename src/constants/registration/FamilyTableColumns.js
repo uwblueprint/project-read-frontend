6e199c170540caeab0954e75f27d1e164022c0ef
Brittany Lau <brittanyplau@gmail.com>
2021-04-30 23:06:39 -0400

    Refactor default fields

--- src/constants/registration/FamilyTableColumns.js
+++ src/constants/registration/FamilyTableColumns.js
@@ -1,39 +1,39 @@
-import DefaultFields from "../DefaultFields";
+import { DefaultFields } from "../DefaultFields";
 
 const FamilyTableColumns = [
   {
-    name: DefaultFields.ID.name,
-    label: DefaultFields.ID.label,
+    name: DefaultFields.ID.id,
+    label: DefaultFields.ID.name,
     options: { display: "excluded" },
   },
   {
-    name: DefaultFields.FIRST_NAME.name,
-    label: DefaultFields.FIRST_NAME.label,
+    name: DefaultFields.FIRST_NAME.id,
+    label: DefaultFields.FIRST_NAME.name,
     options: { filter: false },
   },
   {
-    name: DefaultFields.LAST_NAME.name,
-    label: DefaultFields.LAST_NAME.label,
+    name: DefaultFields.LAST_NAME.id,
+    label: DefaultFields.LAST_NAME.name,
     options: { filter: false },
   },
   {
-    name: DefaultFields.EMAIL.name,
-    label: DefaultFields.EMAIL.label,
+    name: DefaultFields.EMAIL.id,
+    label: DefaultFields.EMAIL.name,
     options: { filter: false },
   },
   {
-    name: DefaultFields.PHONE.name,
-    label: DefaultFields.PHONE.label,
+    name: DefaultFields.PHONE.id,
+    label: DefaultFields.PHONE.name,
     options: { filter: false },
   },
   {
-    name: DefaultFields.NUM_CHILDREN.name,
-    label: DefaultFields.NUM_CHILDREN.label,
+    name: DefaultFields.NUM_CHILDREN.id,
+    label: DefaultFields.NUM_CHILDREN.name,
     options: { searchable: false, filterOptions: { fullWidth: true } },
   },
   {
-    name: DefaultFields.PREFERRED_CONTACT.name,
-    label: DefaultFields.PREFERRED_CONTACT.label,
+    name: DefaultFields.PREFERRED_CONTACT.id,
+    label: DefaultFields.PREFERRED_CONTACT.name,
     options: { searchable: false, filterOptions: { fullWidth: true } },
   },
 ];
