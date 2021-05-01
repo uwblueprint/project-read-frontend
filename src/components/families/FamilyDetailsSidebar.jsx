2e87e52e514f31cf2098e131db102875836c2912
PRANNOY LAL <prannoylal@gmail.com>
2021-05-01 16:41:54 -0400

    fix map

--- src/components/families/FamilyDetailsSidebar.jsx
+++ src/components/families/FamilyDetailsSidebar.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { Drawer } from "@material-ui/core";
+import React, { useEffect, useState, useContext } from "react";
+import { Drawer, Divider } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
+import { FieldsContext } from "../../context/fields";
 import FamilyAPI from "../../api/FamilyAPI";
 
-const drawerWidth = 250;
+const drawerWidth = 400;
 
 const useStyles = makeStyles(() => ({
   drawer: {
@@ -14,9 +15,28 @@ const useStyles = makeStyles(() => ({
     width: drawerWidth,
     padding: 20,
   },
+  divider: {
+    background: "black",
+  },
 }));
 
+// function ParentInfo(parentFields, parentData) {
+//   // parentFields.map((parentField) => console.log(parentField));
+
+//   const parentInfo = parentFields.map((parentField, index) => (
+//     <p key={parentField?.id}>
+//       <b>{parentField?.name}:</b>{" "}
+//       {parentData.parent?.information[`${index - 1}`]}
+//     </p>
+//   ));
+//   return (
+//     <div>
+//       {parentInfo}: {parentData.home_number}
+//     </div>
+//   );
+// }
 function FamilyDetailsSidebar({ isOpen, familyId, handleClose }) {
+  const { parentFields } = useContext(FieldsContext);
   const [family, setFamily] = useState(null);
   const classes = useStyles();
 
@@ -26,6 +46,13 @@ function FamilyDetailsSidebar({ isOpen, familyId, handleClose }) {
     }
     if (familyId) fetchFamily();
   }, [familyId]);
+
+  const parentInfo = parentFields.map((parentField, index) => (
+    <p key={parentField?.id}>
+      <b>{parentField?.name}:</b>{" "}
+      {family?.parent?.information[`${index + 1}`] ?? ""}
+    </p>
+  ));
 
   return (
     <Drawer
@@ -38,10 +65,36 @@ function FamilyDetailsSidebar({ isOpen, familyId, handleClose }) {
       open={isOpen}
       onClose={handleClose}
     >
-      <h3>
-        Hey {family?.parent?.first_name ?? ""} {family?.parent?.last_name ?? ""}
-        !
-      </h3>
+      <h2>
+        {family?.parent?.first_name} {family?.parent?.last_name}
+      </h2>
+      <Divider
+        variant="fullWidth"
+        classes={{
+          root: classes.divider,
+        }}
+      />
+      <h3>Basic Information</h3>
+      {parentInfo}
+      <p>
+        <b>Home phone:</b>
+        {family?.home_number}
+      </p>
+      <p>
+        <b>Cell phone:</b> {family?.cell_number}
+      </p>
+      <p>
+        <b>Work phone:</b> {family?.work_number}
+      </p>
+      <p>
+        <b>Email:</b> {family?.email}
+      </p>
+      <p>
+        <b>Mailing address:</b> {family?.address}
+      </p>
+      <p>
+        <b>Preferred contact:</b> {family?.preferred_comms}
+      </p>
     </Drawer>
   );
 }
