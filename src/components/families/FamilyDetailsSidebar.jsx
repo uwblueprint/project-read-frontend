c5f9ed2ca8fc189784b53255a7d280b21ebf9648
PRANNOY LAL <prannoylal@gmail.com>
2021-05-08 16:05:55 -0400

    addressed pr comments

--- src/components/families/FamilyDetailsSidebar.jsx
+++ src/components/families/FamilyDetailsSidebar.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState, useContext } from "react";
-import { Drawer, Divider } from "@material-ui/core";
+import { Drawer, Divider, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
+import { DefaultFamilyFields } from "../../constants/DefaultFields";
 import { FieldsContext } from "../../context/fields";
 import FamilyAPI from "../../api/FamilyAPI";
 
@@ -15,13 +16,30 @@ const useStyles = makeStyles(() => ({
     width: drawerWidth,
     padding: 20,
   },
-  divider: {
-    background: "black",
+  pb: {
+    paddingBottom: 20,
   },
 }));
 
-function FamilyDetailsSidebar({ isOpen, familyId, handleClose }) {
+function ParentInfo({ family, classes }) {
   const { parentFields } = useContext(FieldsContext);
+  return parentFields.map((parentField) => (
+    <Typography variant="body1" className={classes.pb} key={parentField.id}>
+      <b>{parentField.name}:</b>{" "}
+      {family.parent.information[`${parentField.id}`] ?? ""}
+    </Typography>
+  ));
+}
+
+function DefaultInfo({ family, classes }) {
+  return DefaultFamilyFields.map((defaultField) => (
+    <Typography variant="body1" className={classes.pb} key={defaultField.name}>
+      <b>{defaultField.name}:</b> {family[`${defaultField.id}`] ?? ""}
+    </Typography>
+  ));
+}
+
+function FamilyDetailsSidebar({ isOpen, familyId, handleClose }) {
   const [family, setFamily] = useState(null);
   const classes = useStyles();
 
@@ -32,14 +50,7 @@ function FamilyDetailsSidebar({ isOpen, familyId, handleClose }) {
     if (familyId) fetchFamily();
   }, [familyId]);
 
-  const parentInfo = parentFields.map((parentField) => (
-    <p key={parentField?.id}>
-      <b>{parentField?.name}:</b>{" "}
-      {family?.parent?.information[`${parentField?.id}`] ?? ""}
-    </p>
-  ));
-
-  return (
+  return family ? (
     <Drawer
       anchor="right"
       variant="temporary"
@@ -50,36 +61,27 @@ function FamilyDetailsSidebar({ isOpen, familyId, handleClose }) {
       open={isOpen}
       onClose={handleClose}
     >
-      <h2>
-        {family?.parent?.first_name} {family?.parent?.last_name}
-      </h2>
-      <Divider
-        variant="fullWidth"
-        classes={{
-          root: classes.divider,
-        }}
-      />
-      <h3>Basic Information</h3>
-      <p>
-        <b>Home phone:</b> {family?.home_number}
-      </p>
-      <p>
-        <b>Cell phone:</b> {family?.cell_number}
-      </p>
-      <p>
-        <b>Work phone:</b> {family?.work_number}
-      </p>
-      <p>
-        <b>Email:</b> {family?.email}
-      </p>
-      <p>
-        <b>Mailing address:</b> {family?.address}
-      </p>
-      <p>
-        <b>Preferred contact:</b> {family?.preferred_comms}
-      </p>
-      {parentInfo}
+      <Typography variant="h3">
+        {family.parent.first_name} {family.parent.last_name}
+      </Typography>
+      <Divider variant="fullWidth" />
+      <Typography variant="h5" className="pb">
+        Basic Information
+      </Typography>
+      <DefaultInfo family={family} classes={classes} />
+      <ParentInfo family={family} classes={classes} />
     </Drawer>
+  ) : (
+    <Drawer
+      anchor="right"
+      variant="temporary"
+      className={classes.drawer}
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+      open={isOpen}
+      onClose={handleClose}
+    />
   );
 }
 
