8ac04f91e2d06e431115b9de736b65b3f0727cf3
Brittany Lau <brittanyplau@gmail.com>
2021-05-15 19:32:29 -0400

    Add Typescript

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 import { AppBar, Button, IconButton, Toolbar } from "@material-ui/core";
 import { AccountCircle } from "@material-ui/icons";
-import { makeStyles } from "@material-ui/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(() => ({
   navbar: {
