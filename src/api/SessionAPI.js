131811eeb0cc384fb058380b3615dffa84736617
Lambert Liu <lambert0928@outlook.com>
2021-06-05 00:06:27 -0400

    sessions page tabs

--- src/api/SessionAPI.js
+++ src/api/SessionAPI.js
@@ -2,4 +2,5 @@ import * as APIUtils from "./APIUtils";
 
 export default {
   getSessions: () => APIUtils.get("/sessions"),
+  getClasses: (id) => APIUtils.get(`/sessions/${id}/classes`),
 };
--- src/theme.js
+++ src/theme.js
@@ -51,6 +51,31 @@ const theme = createMuiTheme({
         wordBreak: "break-word",
       },
     },
+    MuiTab: {
+      root: {
+        minWidth: 0,
+        backgroundColor: "#E7E7E7",
+        borderRadius: "15px 15px 0px 0px",
+        opacity: "100%",
+        border: "1px solid #C8C8C8",
+        borderBottom: "0px",
+        fontWeight: 700,
+        textTransform: "none",
+        "@media (min-width: 0px)": {
+          minWidth: 0,
+        },
+        "&$selected": {
+          backgroundColor: "white",
+        },
+      },
+    },
+    MuiTabs: {
+      root: {
+        backgroundColor: "white",
+        color: "black",
+        borderBottom: "1px solid #C8C8C8",
+      },
+    },
   },
   typography: {
     h1: {
