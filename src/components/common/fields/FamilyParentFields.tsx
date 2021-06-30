942530724ff0fbf0083a896eb6499734eba139ed
brittanylau <brittanyplau@gmail.com>
2021-06-30 00:00:08 -0400

    clean up spacing

--- src/components/common/fields/FamilyParentFields.tsx
+++ src/components/common/fields/FamilyParentFields.tsx
@@ -19,91 +19,50 @@ const FamilyFields = ({ family, dynamicFields, onChange }: Props) => (
       field={{ ...DefaultFields.FIRST_NAME, role: StudentRole.PARENT }}
       value={family.parent.first_name}
       onChange={(value) =>
-        onChange({
-          ...family,
-          parent: { ...family.parent, first_name: value },
-        })
+        onChange({ ...family, parent: { ...family.parent, first_name: value } })
       }
     />
     <FieldInput
       field={{ ...DefaultFields.LAST_NAME, role: StudentRole.PARENT }}
       value={family.parent.last_name}
       onChange={(value) =>
-        onChange({
-          ...family,
-          parent: { ...family.parent, last_name: value },
-        })
+        onChange({ ...family, parent: { ...family.parent, last_name: value } })
       }
     />
     <FieldInput
       field={{ ...DefaultFields.HOME_NUMBER, role: StudentRole.PARENT }}
       value={family.home_number}
-      onChange={(value) =>
-        onChange({
-          ...family,
-          home_number: value,
-        })
-      }
+      onChange={(value) => onChange({ ...family, home_number: value })}
     />
     <FieldInput
       field={{ ...DefaultFields.CELL_NUMBER, role: StudentRole.PARENT }}
       value={family.cell_number}
-      onChange={(value) =>
-        onChange({
-          ...family,
-          cell_number: value,
-        })
-      }
+      onChange={(value) => onChange({ ...family, cell_number: value })}
     />
     <FieldInput
       field={{ ...DefaultFields.WORK_NUMBER, role: StudentRole.PARENT }}
       value={family.work_number}
-      onChange={(value) =>
-        onChange({
-          ...family,
-          work_number: value,
-        })
-      }
+      onChange={(value) => onChange({ ...family, work_number: value })}
     />
     <FieldInput
       field={{ ...DefaultFields.PREFERRED_NUMBER, role: StudentRole.PARENT }}
       value={family.preferred_number}
-      onChange={(value) =>
-        onChange({
-          ...family,
-          preferred_number: value,
-        })
-      }
+      onChange={(value) => onChange({ ...family, preferred_number: value })}
     />
     <FieldInput
       field={{ ...DefaultFields.EMAIL, role: StudentRole.PARENT }}
       value={family.email}
-      onChange={(value) =>
-        onChange({
-          ...family,
-          email: value,
-        })
-      }
+      onChange={(value) => onChange({ ...family, email: value })}
     />
     <FieldInput
       field={{ ...DefaultFields.ADDRESS, role: StudentRole.PARENT }}
       value={family.address}
-      onChange={(value) =>
-        onChange({
-          ...family,
-          address: value,
-        })
-      }
+      onChange={(value) => onChange({ ...family, address: value })}
     />
     <FieldInput
       field={{ ...DefaultFields.PREFERRED_CONTACT, role: StudentRole.PARENT }}
       value={family.preferred_comms}
-      onChange={(value) =>
-        onChange({
-          ...family,
-          preferred_comms: value,
-        })
-      }
+      onChange={(value) => onChange({ ...family, preferred_comms: value })}
     />
     {dynamicFields.map((field) => (
       <FieldInput
