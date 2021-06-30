9918557042b7131a29a09d45a74e92f2d3f5c9fa
brittanylau <brittanyplau@gmail.com>
2021-06-29 23:41:03 -0400

    improve type safety & reusability for reg form fields

---
+++ src/components/common/fields/StudentFields.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { DynamicField } from "../../../types";
+import FieldInput from "./FieldInput";
+import { DefaultFields } from "../../../constants/DefaultFields";
+import StudentRole from "../../../constants/StudentRole";
+import { StudentRequest } from "../../../api/FamilyAPI";
+import DefaultFieldKey from "../../../constants/DefaultFieldKey";
+
+export const defaultStudentData: StudentRequest = {
+  [DefaultFieldKey.FIRST_NAME]: "",
+  [DefaultFieldKey.LAST_NAME]: "",
+  information: {},
+};
+
+type Props = {
+  student: StudentRequest;
+  role: StudentRole;
+  dynamicFields: DynamicField[];
+  onChange: (data: StudentRequest) => void;
+};
+
+const StudentFields = ({ student, role, dynamicFields, onChange }: Props) => (
+  <>
+    <FieldInput
+      field={{ ...DefaultFields.FIRST_NAME, role }}
+      value={student.first_name}
+      onChange={(value) =>
+        onChange({
+          ...student,
+          first_name: value,
+        })
+      }
+    />
+    <FieldInput
+      field={{ ...DefaultFields.LAST_NAME, role }}
+      value={student.last_name}
+      onChange={(value) =>
+        onChange({
+          ...student,
+          last_name: value,
+        })
+      }
+    />
+    {dynamicFields.map((field) => (
+      <FieldInput
+        key={field.id}
+        field={field}
+        value={student.information[field.id] ?? ""}
+        onChange={(value) =>
+          onChange({
+            ...student,
+
+            information: {
+              ...student.information,
+              [field.id]: value,
+            },
+          })
+        }
+      />
+    ))}
+  </>
+);
+export default StudentFields;
