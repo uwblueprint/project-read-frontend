82322e833522f37cb49bae96d52596557f11c7ee
Tayef Shah <tayefshah@gmail.com>
2021-07-06 23:55:36 -0400

    move and reorder family table enrolment fields

--- src/pages/MainRegistration.tsx
+++ src/pages/MainRegistration.tsx
@@ -2,19 +2,13 @@ import React, { useEffect, useState } from "react";
 
 import { Typography } from "@material-ui/core";
 
-import FamilyAPI from "api/FamilyAPI";
-import { FamilyListResponse } from "api/types";
-import FamilyTable from "components/families/FamilyTable";
-import { DefaultFields } from "constants/DefaultFields";
-import { DefaultField } from "types";
+import FamilyAPI from "../api/FamilyAPI";
+import { FamilyListResponse } from "../api/types";
+import FamilyTable from "../components/families/FamilyTable";
+import { DefaultFields } from "../constants/DefaultFields";
 
 const MainRegistration = () => {
   const [families, setFamilies] = useState<FamilyListResponse[]>([]);
-  const FamilyEnrolmentFields: DefaultField[] = [
-    DefaultFields.IS_ENROLLED,
-    DefaultFields.CURRENT_PREFERRED_CLASS,
-    DefaultFields.CURRENT_CLASS,
-  ];
 
   useEffect(() => {
     async function fetchFamilies() {
@@ -28,7 +22,11 @@ const MainRegistration = () => {
       <Typography variant="h1">Main registration</Typography>
       <FamilyTable
         families={families}
-        enrolmentFields={FamilyEnrolmentFields}
+        enrolmentFields={[
+          DefaultFields.CURRENT_PREFERRED_CLASS,
+          DefaultFields.IS_ENROLLED,
+          DefaultFields.CURRENT_CLASS,
+        ]}
         shouldDisplayDynamicFields
       />
     </>
