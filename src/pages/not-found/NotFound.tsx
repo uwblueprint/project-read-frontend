f7bf19f901f945c9a17225bd742721485438cf2a
brittanylau <brittanyplau@gmail.com>
2021-08-14 16:00:50 -0400

    clean up styling

--- src/pages/not-found/NotFound.tsx
+++ src/pages/not-found/NotFound.tsx
@@ -5,22 +5,27 @@ import Box from "@material-ui/core/Box";
 import { ErrorOutline } from "@material-ui/icons";
 import { Link } from "react-router-dom";
 
-const NotFound = () => (
-  <Box
-    textAlign="center"
-    position="absolute"
-    top="50%"
-    left="50%"
-    style={{ transform: "translate(-50%, -50%)" }}
-  >
-    <Box>
-      <ErrorOutline fontSize="large" />
+import useStyles from "./styles";
+
+const NotFound = () => {
+  const classes = useStyles();
+  return (
+    <Box
+      textAlign="center"
+      position="absolute"
+      top="50%"
+      left="50%"
+      className={classes.container}
+    >
+      <Box>
+        <ErrorOutline fontSize="large" />
+      </Box>
+      <h1>Oh no! This page doesn&apos;t exist.</h1>
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <Button color="primary">Return to all registrations</Button>
+      </Link>
     </Box>
-    <h1>Oh no! This page doesn&apos;t exist.</h1>
-    <Link to="/" style={{ textDecoration: "none" }}>
-      <Button color="primary">Return to all registrations</Button>
-    </Link>
-  </Box>
-);
+  );
+};
 
 export default NotFound;
