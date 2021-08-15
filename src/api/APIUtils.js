f2aef917587ed2d1df535bd605f6ea083a9f95d2
Daniel <daniel.chen115@gmail.com>
2021-08-15 01:10:35 -0400

    Signs out user during the first request they make of every day

---
+++ src/api/APIUtils.js
@@ -0,0 +1,43 @@
+import app from "firebase/config";
+
+const url = process.env.REACT_APP_API_URL;
+
+async function getIdToken() {
+  const user = app.auth().currentUser;
+  if (!user) {
+    return "";
+  }
+  const idTokenResult = await user.getIdTokenResult();
+  const lastLoggedIn = new Date(idTokenResult.authTime).getDate();
+  if (lastLoggedIn !== new Date().getDate()) {
+    await app.auth().signOut();
+    return "";
+  }
+
+  return user.getIdToken();
+}
+
+export async function get(path) {
+  const token = await getIdToken();
+  const res = await fetch(url + path, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return res.json();
+}
+
+export async function post(path, data) {
+  const token = await getIdToken();
+  const res = await fetch(url + path, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  return res.json();
+}
