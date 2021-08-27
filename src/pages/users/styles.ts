c176666e4132de22bf3868231c0ede36f81e769c
Lambert Liu <lambert0928@outlook.com>
2021-08-26 22:33:36 -0400

    post users dialog

--- src/pages/users/styles.ts
+++ src/pages/users/styles.ts
@@ -1,12 +1,28 @@
+import { Theme } from "@material-ui/core";
+import { grey } from "@material-ui/core/colors";
 import { makeStyles } from "@material-ui/styles";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme: Theme) => ({
   activationButton: {
     fontWeight: 400,
     textDecoration: "underline",
     "&:hover": {
       textDecoration: "underline",
     },
+  },
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+  },
+  input: {
+    fontSize: 14,
+    height: 32,
+    paddingBottom: 0,
+    paddingTop: 0,
+  },
+  registerButton: {
+    marginLeft: "20px",
   },
   roleSelect: {
     fontSize: 14,
@@ -17,6 +33,9 @@ const useStyles = makeStyles(() => ({
   roleSelectTabelCell: {
     paddingBottom: 0,
     paddingTop: 0,
+  },
+  selectPlaceholder: {
+    color: grey[500],
   },
   tableContainer: {
     marginTop: 32,
